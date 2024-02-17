68c61f55a4647ac53f38cb5a9d755f498cf3aeb7
yusufkhan08070817 <khanyusuk340@gmail.com>
2024-02-17 14:49:39 +0530

    first

---
+++ CustomNave/src/main/java/com/example/customnave/classes/navedata.kt
@@ -0,0 +1,8 @@
+package com.example.task.classes
+
+import android.widget.ImageButton
+
+data class navedata(val imageButton1: ImageButton,val imageButton2: ImageButton,val imageButton3: ImageButton,val imageButton4: ImageButton,val imageButton5: ImageButton,)
+object naveobj{
+    lateinit var naveobj:navedata
+}
---
+++ app/src/main/java/com/example/customnave/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.customnave
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.RelativeLayout
+import com.example.task.classes.Cnave
+import com.example.task.classes.Nave
+import com.example.task.classes.naveobj
+import com.example.task.classes.state
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Cnave(this)
+        Nave.getInstance(this.applicationContext)
+        val viewGroup:RelativeLayout=findViewById(R.id.abc)
+        Nave.add(viewGroup)
+        naveobj.naveobj.imageButton1.setImageResource(R.drawable.ic_launcher_foreground)
+
+    }
+}
